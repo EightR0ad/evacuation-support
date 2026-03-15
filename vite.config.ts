6efc978287ac9ed131ff332620e67b5bf6dd6867
EightR0ad <438699628@qq.com>
2026-03-15 11:41:42 +0000

    fix: vite build dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/evacuation-support/",
   plugins: [react(), cesium({
     rebuildCesium: false,
   })],
